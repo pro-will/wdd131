d4a861002b307aa18f3384914b3ee5a97a99d260
William Pelo <wlpelo9@gmail.com>
2025-09-18 23:31:55 +0100

    make change

---
+++ scripts/place.js
@@ -0,0 +1,3 @@
+// Footer Year + Last Modified
+    document.getElementById("year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
